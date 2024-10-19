4d0a0dffabf1a940da3eee074431dbeedc0b3475
khalidhub7 <louguikhalid7@gmail.com>
2024-10-18 21:47:20 -0700

    lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,10 @@
--- Old school band 
--- SQL script that lists all bands with Glam rock as their main style
-
-
-SET @this_year := 2022;
-
-SELECT band_name, IF(split IS NULL, @this_year, split) - formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%'
+-- calcule lifespan of the band
+-- if it still working use 2022 as it end
+SELECT band_name, 
+       CASE 
+           WHEN split IS NULL THEN 2022 - YEAR(formed)  -- Band still active, use 2022
+           ELSE YEAR(split) - YEAR(formed)              -- Band broke up, use split year
+       END AS lifespan
+FROM metal_bands
+WHERE style = "Glam rock"
 ORDER BY lifespan DESC;
