df44ad9f3fad9001f3f57ca568aecaea2fb9d246
khalidhub7 <louguikhalid7@gmail.com>
2024-10-18 23:03:02 -0700

    lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,14 @@
 -- calcule lifespan of the band
 -- if it still working use 2022 as it end
+
 SELECT band_name, 
        CASE 
-           WHEN split IS NULL THEN 2022 - YEAR(formed)  -- Band still active, use 2022
-           ELSE YEAR(split) - YEAR(formed)              -- Band broke up, use split year
+           WHEN formed IS NULL THEN 0
+           -- Band still active, use 2022
+           WHEN split IS NULL THEN 2022 - formed
+           -- Band broke up, use split year
+           ELSE split - formed
        END AS lifespan
 FROM metal_bands
-WHERE style = "Glam rock"
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
