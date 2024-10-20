464e02992ce136b993695eb98488fb7481085c31
khalidhub7 <louguikhalid7@gmail.com>
2024-10-19 21:43:32 -0700

    trigger email ###

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,14 @@
 -- trigger valid_email column
 -- if email is changed
 DELEMITER ::
+
 CREATE TRIGGER emailvalid
 BEFORE UPDATE ON users
 FOR EACH ROW
 SET NEW.valid_email = IF(
     NEW.email = OLD.email, NEW.valid_email, 0
-    )
+    );
+
 ::
+
 DELEMITER ;
