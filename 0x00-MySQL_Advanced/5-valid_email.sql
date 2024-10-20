129e1dd03e6c5090f52090e46dfc737af2fea560
khalidhub7 <louguikhalid7@gmail.com>
2024-10-19 21:33:35 -0700

    trigger email ###

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,15 @@
 -- trigger if an email changed
 DROP TRIGGER IF EXISTS reset_email;
 DELIMITER //
+
 CREATE TRIGGER reset_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    ELSE
-        SET NEW.valid_email = 1;
-    END IF;
-END; //
+	IF NEW.email <> OLD.email THEN
+	       	SET NEW.valid_email = 0;
+	ELSE
+		SET NEW.valid_email = 1;
+	END IF;
+//
+
 DELIMITER ;
