b01290dd4bf0bb4ee1698ccf9c086ef58f7b6fff
khalidhub7 <louguikhalid7@gmail.com>
2024-10-20 17:58:03 -0700

    check meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,8 @@
--- No table for a meeting 
--- SQL script that creates a view need_meeting
-CREATE VIEW need_meeting AS
-SELECT name FROM students
-WHERE score < 80
-AND (last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH) OR last_meeting IS NULL) 
+-- creates an view
+
+CREATE VIEW need_meeting
+AS name * FROM students
+WHERE students.score < 80
+AND (last_meeting IS NULL
+OR last_meeting < DATE_SUB(
+CURDATE(), INTERVAL 1 MONTH))
