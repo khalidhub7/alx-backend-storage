a8408aa68a493deee0beffba2ea4a90d32d2c8e3
khalidhub7 <louguikhalid7@gmail.com>
2024-10-22 07:14:10 -0700

    upp

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
-"""List all documents in Python """
-import pymongo
+'''Task 14's module.
+'''
 
 
 def top_students(mongo_collection):
-    """averageScore"""
-    stus = mongo_collection.find()
-    sorted_stud = []
-    for s in stus:
-        sum = 0
-        average = {}
-        average = s
-        for t in s["topics"]:
-            sum += t["score"]
-        average["averageScore"] = sum / len(s["topics"])
-        sorted_stud.append(average)
-    return sorted(sorted_stud, key=lambda x: x["averageScore"], reverse=True)
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
