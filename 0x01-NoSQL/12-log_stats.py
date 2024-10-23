e344543e68b485bb2c95cec2b13dcca1b7b66071
khalidhub7 <louguikhalid7@gmail.com>
2024-10-22 18:47:11 -0700

    pymongo: ngiii

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python3
-"""Script that provides stats about Nginx logs stored in MongoDB"""
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
-
-
-def log_stats():
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
+import pymongo
+from pymongo.errors import PyMongoError, ServerSelectionTimeoutError
+if __name__ == "__main__":
     try:
-        client = MongoClient(
-            'mongodb://127.0.0.1:27017',
+        myclient = pymongo.MongoClient(
+            "mongodb://127.0.0.1:27017",
             serverSelectionTimeoutMS=5000)
-        client.server_info()
-    except (ConnectionFailure, ServerSelectionTimeoutError) as e:
-        print(f"Unable to connect to the server: {e}")
-        return
-    db = client.get_database("logs")
-    nginx_collection = db.get_collection("nginx")
-    try:
-        log_count = nginx_collection.count_documents({})
-        print(f"{log_count} logs")
+        mydb = myclient.logs
+        mycol = mydb.nginx
+        myclient.admin.command('ping')
+        print("{} logs".format(mycol.count_documents({})))
         print("Methods:")
         methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        for method in methods:
-            method_count = nginx_collection.count_documents({"method": method})
-            print(f"\tmethod {method}: {method_count}")
-        status_count = nginx_collection.count_documents(
-            {"method": "GET", "path": "/status"})
-        print(f"{status_count} status check")
-    except Exception as e:
-        print(f"Error while fetching stats from the database: {e}")
-
-
-if __name__ == "__main__":
-    log_stats()
+        for m in methods:
+            print("\tmethod {}: {}".format(
+                m, mycol.count_documents({"method": m})))
+        print("{} status check".format(mycol.count_documents(
+            {"method": "GET", "path": "/status"})))
+    except (ServerSelectionTimeoutError, PyMongoError):
+        print("Error: Unable to connect to MongoDB.")
