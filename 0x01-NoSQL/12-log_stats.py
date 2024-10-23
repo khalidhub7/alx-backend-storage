f60aff6f841d4ee24db0147a135e05eeb8e38b52
khalidhub7 <louguikhalid7@gmail.com>
2024-10-22 18:49:09 -0700

    pymongo: ngiii

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 """
 Python script that provides some stats
-about Nginx logs stored in MongoDB
-"""
+about Nginx logs stored in MongoDB """
 import pymongo
-from pymongo.errors import PyMongoError, ServerSelectionTimeoutError
 if __name__ == "__main__":
-    try:
-        myclient = pymongo.MongoClient(
-            "mongodb://127.0.0.1:27017",
-            serverSelectionTimeoutMS=5000)
-        mydb = myclient.logs
-        mycol = mydb.nginx
-        myclient.admin.command('ping')
-        print("{} logs".format(mycol.count_documents({})))
-        print("Methods:")
-        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        for m in methods:
-            print("\tmethod {}: {}".format(
-                m, mycol.count_documents({"method": m})))
-        print("{} status check".format(mycol.count_documents(
+    myclient = pymongo.MongoClient(
+        "mongodb://127.0.0.1:27017")
+    mydb = myclient.logs
+    mycol = mydb.nginx
+    print("{} logs".format(
+        mycol.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print("\tmethod {}: {}".format(
+            m, mycol.count_documents({"method": m})))
+    print("{} status check".format(
+        mycol.count_documents(
             {"method": "GET", "path": "/status"})))
-    except (ServerSelectionTimeoutError, PyMongoError):
-        print("Error: Unable to connect to MongoDB.")
