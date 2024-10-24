ef67ff555f9527f95e54ce2dfb654bacb4dbc088
khalidhub7 <louguikhalid7@gmail.com>
2024-10-24 10:23:06 -0700

    declare key value in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,9 +10,8 @@ class Cache:
 
     def __init__(self):
         """ constructor """
-        self._redis = redis.Redis(
-            host='localhost', port=6379)
-        self._redis.flushdb(True)
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(
             self, data: Union[
