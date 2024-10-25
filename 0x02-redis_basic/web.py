bc3ac8a0855249b0bea74dabfddb7cde088784b1
khalidhub7 <louguikhalid7@gmail.com>
2024-10-25 13:25:17 -0700

    up

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,21 @@
-
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+""" web cache and tracker """
 import requests
 import redis
-from functools import wraps
 
 store = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
-
-
-@count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
-
-
-if __name__ == "__main__":
-    print(get_page("http://slowwly.robertomurray.co.uk"))
+    """ Returns HTML content of a URL and tracks URL access count in Redis """
+    count_key = f"count:{url}"
+    cached_key = f"cached:{url}"
+    
+    cached_data = store.get(cached_key)
+    if cached_data:
+        return cached_data.decode("utf-8")
+    
+    html = requests.get(url).text
+    store.incr(count_key)
+    store.setex(cached_key, 10, html)
+    return html
