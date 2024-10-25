9ae5a9c5269542a3f468ca5fb9a9f6542f4ab1f9
khalidhub7 <louguikhalid7@gmail.com>
2024-10-25 13:26:44 -0700

    up

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,15 +7,23 @@ store = redis.Redis()
 
 
 def get_page(url: str) -> str:
-    """ Returns HTML content of a URL and tracks URL access count in Redis """
+    """ Returns HTML content of
+    a URL and tracks URL access count in Redis """
     count_key = f"count:{url}"
     cached_key = f"cached:{url}"
-    
+
     cached_data = store.get(cached_key)
     if cached_data:
         return cached_data.decode("utf-8")
-    
+
     html = requests.get(url).text
     store.incr(count_key)
     store.setex(cached_key, 10, html)
     return html
+
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.u\
+        k/delay/3000/url/http://www.example.com"
+    print(get_page(url))
+
