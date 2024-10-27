b2ffabae87cb2785de163f8aa41bd842e4507016
khalidhub7 <louguikhalid7@gmail.com>
2024-10-26 17:07:52 -0700

    ffix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,29 +13,29 @@ def count_requests(
 count requests decorator
     """
     @wraps(method)
-    def wrapper(url: str) -> Callable:
+    def wrapper(url: str) -> str:
         """ wrapper function """
         r = method.__self__._redis
         r.incr("count:{}"
                .format(str(url)))
         cached_html = r.get(
-            str("cached:{}".format(str(url))))
+            "cached:{}".format(str(url)))
         if cached_html:
-            return str(cached_html.decode(
-                'utf-8'))
-        html = str(method(str(url)).decode(
-            'utf-8'))
+            return cached_html.decode(
+                'utf-8')
+        html = method(str(url)).decode(
+            'utf-8')
         r.setex("cached:{}"
-                .format(str(url)), 10, html)
-        return str(r.get(
+                .format(url), 10, html)
+        return r.get(
             "cached:{}".format(str(url))
-        ).decode('utf-8'))
+        ).decode('utf-8')
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
     """ get page """
-    r = requests.get(str(url))
-    return str(r.text.decode(
-        'utf-8'))
+    r = requests.get(url)
+    return r.text.decode(
+        'utf-8')
