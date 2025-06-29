efb4e7fa9c6d93e92e2ac97a4c145f159eaae128
khalidhub7 <louguikhalid7@gmail.com>
2025-06-29 11:50:39 +0100

    best band country

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,9 @@
--- create table users
-SELECT origin, sum(fans) AS nb_fans
-FROM metal_bands
--- required: Without it MySQL doesn’t know
--- how to group the data and will throw an error
-GROUP BY origin
--- optional
-ORDER BY nb_fans DESC;
+SELECT
+    origin,
+    count(*) nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
