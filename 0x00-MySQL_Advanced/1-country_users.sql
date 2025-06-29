92d4c4a5e8853949cdfd976c23140a6638537d93
khalidhub7 <louguikhalid7@gmail.com>
2025-06-29 02:40:54 +0100

    basic table creation #

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
-        country ENUM ('US', 'CO', 'TN') DEFAULT 'US'
+        country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
     );
