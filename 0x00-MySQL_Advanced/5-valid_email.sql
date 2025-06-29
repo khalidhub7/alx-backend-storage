ea1a8703edca87d7083cb8e070563df5713d4ae3
khalidhub7 <louguikhalid7@gmail.com>
2025-06-29 22:16:06 +0100

    reset valid_email if email changes

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,9 +6,8 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
-        UPDATE users
-        SET valid_email = 0
-        WHERE id = NEW.id;
+        SET NEW.valid_email = 0;
     END IF;
-END ..
+END;
+..
 DELIMITER ;
