6d968ed034d34520d4568042036a68da2c83f9ab
khalidhub7 <louguikhalid7@gmail.com>
2025-06-29 20:15:36 +0100

    decrease item quantity after insert

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,11 @@
--- update quantity when new order inserted
-DELIMITER //
-CREATE TRIGGER update_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END; //
+-- decrease item quantity after insert
+DELIMITER ..
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders FOR EACH ROW BEGIN
+UPDATE items
+SET
+    quantity = quantity - 1
+WHERE
+    name = NEW.item_name;
+
+END ..
 DELIMITER ;
