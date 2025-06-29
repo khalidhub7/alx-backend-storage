5461644a112d984962c5e831bd2b7d3170c42602
khalidhub7 <louguikhalid7@gmail.com>
2025-06-29 13:58:08 +0100

    glam rock bands by lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,13 @@
 -- glam rock bands by lifespan
 SELECT
     band_name,
-    (split - formed) AS lifespan
+    (IFNULL (split, 2022) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
     -- e.g. FIND_IN_SET('Glam rock', 'Heavy metal,Glam rock,Hard rock')
-    FIND_IN_SET ('Glam rock', REPLACE (style, ', ', ','))
-    AND formed IS NOT NULL
+    -- FIND_IN_SET ('Glam rock', REPLACE (style, ', ', ','))
+    -- AND formed IS NOT NULL
+    style LIKE '%Glam rock%'
 ORDER BY
     lifespan DESC
