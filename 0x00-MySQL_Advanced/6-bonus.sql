9d7f483f2ea48cd94ccbea4db7715fdd1279f623
khalidhub7 <louguikhalid7@gmail.com>
2025-07-01 14:10:13 +0100

    adds a new correction for a student.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,38 +1,32 @@
--- Add new row correction
+-- add new row correction
 DELIMITER ..
 
 CREATE PROCEDURE AddBonus(
-    IN user_id INT, 
-    IN project_name VARCHAR(255), 
-    IN score INT
+    IN user_id INT, IN project_name VARCHAR(255), IN score INT
 )
 BEGIN
     DECLARE project_id INT;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
-    BEGIN
-        -- Insert project if it doesn't exist
-        INSERT INTO projects (name) 
-        VALUES (project_name);
-
-        -- Get the project ID
-        SELECT id 
-        INTO project_id 
-        FROM projects 
-        WHERE name = project_name;
-
-        -- Insert correction
-        INSERT INTO corrections (user_id, project_id) 
-        VALUES (user_id, project_id);
-    END;
-
-    -- Get the project ID
-    SELECT id 
-    INTO project_id 
-    FROM projects 
+    -- get the project ID
+    SELECT id INTO project_id FROM projects 
     WHERE name = project_name;
 
-    -- Insert correction with score
+    IF project_id == NULL
+    THEN
+        -- insert project if it doesn't exist
+        INSERT INTO projects (name) VALUES (project_name);
+
+        -- get the project ID
+        SELECT id INTO project_id FROM projects 
+        WHERE name = project_name;
+
+        -- insert correction
+        INSERT INTO corrections (user_id, project_id) 
+        VALUES (user_id, project_id);
+    END IF
+
+
+    -- insert correction with score
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_id, score);
 
