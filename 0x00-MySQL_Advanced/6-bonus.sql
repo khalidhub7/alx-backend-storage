5a63f739f64a52ebb445526193c3b80f9ef24d5f
khalidhub7 <louguikhalid7@gmail.com>
2025-07-01 18:32:36 +0100

    add new correction for a student

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,23 +6,17 @@ CREATE PROCEDURE AddBonus(
 )
 BEGIN
     DECLARE project_id INT;
-
-    -- get project_id if project exists
-    SELECT id INTO project_id FROM projects 
-    WHERE name = project_name LIMIT 1;
+    SET project_id = (
+        SELECT id FROM projects WHERE name = project_name LIMIT 1
+        );
 
     IF project_id IS NULL THEN
-        -- insert new project if not found
         INSERT INTO projects (name) VALUES (project_name);
-
-        -- retrieve last inserted project_id
         SET project_id = LAST_INSERT_ID();
     END IF;
 
-    -- insert correction with the score
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_id, score);
-
 END
 ..
 
