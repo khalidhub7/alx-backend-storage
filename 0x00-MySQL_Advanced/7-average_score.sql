288809cea034451c4bfe0cd7dd9d9fc9eab4eaf8
khalidhub7 <louguikhalid7@gmail.com>
2025-07-02 13:11:44 +0100

    -- updates user's average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,8 +11,6 @@ BEGIN
         SELECT AVG(score) FROM corrections WHERE user_id = userid
     );
 
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = userid;
+    UPDATE users SET average_score = avg_score WHERE id = userid;
 END ..
 DELIMITER ;
