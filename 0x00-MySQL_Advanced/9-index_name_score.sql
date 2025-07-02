5683cd93f1247de4800e7b15c733d719ae0bb969
khalidhub7 <louguikhalid7@gmail.com>
2025-07-02 15:24:52 +0100

    index on first letter of name and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,2 @@
--- creates an index
-
-CREATE INDEX idx_name_first_score ON names(name(1), score);
+-- index on first letter of name and score
+CREATE INDEX idx_name_first_score ON names (name (1), score)
