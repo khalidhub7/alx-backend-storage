d461812a839de5443116a7bd0bec57353d5893dc
khalidhub7 <louguikhalid7@gmail.com>
2025-07-02 21:01:38 +0100

    students needing a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,22 @@
--- creates an view
+-- students needing a meeting
 
+-- returns 1 if no recent meeting
+DELIMITER ..
+CREATE FUNCTION is_met_recently(last_meeting DATE)
+RETURNS BOOLEAN DETERMINISTIC
+BEGIN
+    IF last_meeting IS NULL 
+    OR last_meeting < NOW() - INTERVAL 1 MONTH 
+    THEN
+        RETURN 1;
+    END IF;
+
+    RETURN 0;
+END ..
+DELIMITER ;
+
+-- view of students with low score and no recent meeting
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT name
-FROM students
-WHERE score < 80
-AND (last_meeting IS NULL 
-     OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+SELECT name FROM students
+WHERE score < 80 AND is_met_recently(last_meeting);
