6f28e433875796f5f549dad04209738938adf524
khalidhub7 <louguikhalid7@gmail.com>
2025-07-10 00:05:08 +0100

    all docs from a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 """ pymongo basics """
-from typing import List
 
 
-def list_all(
-        mongo_collection) -> List[dict]:
-    """ lists all documents in a collection """
-    docs = []
-    for i in mongo_collection.find():
-        docs.append(i)
-    if len(docs) == 0:
-        return []
-    else:
-        return docs
+def list_all(mongo_collection):
+    """ return all docs from a collection """
+
+    """ client = MongoClient()  # use default host, port
+    db = client['test']  # test is default db
+    collection = db[mongo_collection]  # my collection """
+
+    return list(mongo_collection.find())  # all docs
