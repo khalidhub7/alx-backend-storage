baf291ced58cdfed06b0a4900288e180f1f4a819
khalidhub7 <louguikhalid7@gmail.com>
2025-07-10 00:15:54 +0100

    all docs from a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,8 +5,8 @@
 def list_all(mongo_collection):
     """ return all docs from a collection """
 
-    """ client = MongoClient()  # use default host, port
-    db = client['test']  # test is default db
-    collection = db[mongo_collection]  # my collection """
+    # client = MongoClient()  # use default host, port
+    # db = client['test']  # test is default db
+    # collection = db[mongo_collection]  # my collection
 
     return list(mongo_collection.find())  # all docs
