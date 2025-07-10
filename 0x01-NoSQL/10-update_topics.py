36511dfcfa0d87484c1188c7f9c841a976824b4d
khalidhub7 <louguikhalid7@gmail.com>
2025-07-10 18:46:18 +0100

    update user topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 """ pymongo basics """
-import pymongo
 
 
-def update_topics(
-        mongo_collection, name, topics):
-    """
-changes all topics of a document
-based on name
-    """
+def update_topics(mongo_collection, name, topics):
+    """ update user topics """
     mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
