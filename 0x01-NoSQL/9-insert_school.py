316b7f9a19a1752059b373abb2e99d2043de54a3
khalidhub7 <louguikhalid7@gmail.com>
2025-07-10 17:29:12 +0100

    insert new doc

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,11 +2,6 @@
 """ pymongo basics """
 
 
-def insert_school(
-        mongo_collection, **kwargs):
-    """
-insert one document in a collection
-    """
-    result = mongo_collection.insert_one(
-        kwargs)
-    return result.inserted_id
+def insert_school(mongo_collection, **kwargs):
+    """ insert new doc """
+    return mongo_collection.insert_one({**kwargs})
