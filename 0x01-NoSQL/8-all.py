46a3c601a76ea2b53b74e9e6587b3ad9cbf66f1f
khalidhub7 <louguikhalid7@gmail.com>
2025-07-12 16:40:34 +0100

    retrieve all docs from a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,8 +5,9 @@
 def list_all(mongo_collection):
     """ return all docs from a collection """
 
-    # client = MongoClient()  # use default host, port
-    # db = client['test']  # test is default db
-    # collection = db[mongo_collection]  # my collection
+    # this is already in main.py
+    # client = MongoClient()     # use default host, port
+    # db = client['test']        # test is default db
+    # collection = db['school']  # my collection
 
     return list(mongo_collection.find())  # all docs
