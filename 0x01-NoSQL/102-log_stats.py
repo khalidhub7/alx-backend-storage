27c5855df95418e382ae15334d91e1eeaed45ca3
khalidhub7 <louguikhalid7@gmail.com>
2025-07-12 16:11:56 +0100

    imporve: most present IPs in the collection

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,27 +1,54 @@
 #!/usr/bin/env python3
-""" Provides some stats about Nginx logs stored in MongoDB """
+""" pymongo basics """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-    nginx_logs = client.logs.nginx
-    print("{} logs".format(nginx_logs.count_documents({})))
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        print("\tmethod {}: {}".format(
-            method,
-            nginx_logs.count_documents({"method": method})
-        ))
-    print("{} status check".format(
-        nginx_logs.count_documents({"method": "GET", "path": "/status"})
-    ))
-    print("IPs:")
-    ips = nginx_logs.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
+def show_logs():
+    """ shows nginx logs stats from mongodb """
+    # make sure to restore backup
+    # mongorestore dump
+
+    # connect to logs.nginx collection
+    client = MongoClient()
+    db = client['logs']
+    collection = db['nginx']
+
+    # count all docs and GET /status logs
+    docs_len = collection.count_documents({})
+    status_check = collection.count_documents(
+        {'path': {'$in': ['/status']}, 'method': 'GET'}
+    )
+
+    # count docs per HTTP method
+    methods_count = {
+        "GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0
+    }
+    for k in methods_count:
+        methods_count[k] = collection.count_documents(
+            {'method': k}
+        )
+
+    # print log counts and method stats
+    print(
+        f"{docs_len} logs\n"
+        f"Methods:\n"
+        f"\tmethod GET: {methods_count['GET']}\n"
+        f"\tmethod POST: {methods_count['POST']}\n"
+        f"\tmethod PUT: {methods_count['PUT']}\n"
+        f"\tmethod PATCH: {methods_count['PATCH']}\n"
+        f"\tmethod DELETE: {methods_count['DELETE']}\n"
+        f"{status_check} status check"
+    )
+
+    # most present IPs in the collection
+    top_ten = collection.aggregate([
+        {'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}}, {'$limit': 10}
     ])
-    for ip in ips:
-        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
+    print('IPs:')
+    for i in top_ten:
+        print(f"\t{i['_id']}: {i['count']}")
+
+
+if __name__ == "__main__":
+    show_logs()
